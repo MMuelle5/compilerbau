1fa94ff4e385572e34373fe60b6850992f7b04c6
Marius Mueller <mmuelle5@hsz-t.ch>
2013-06-12 18:41:15 +0200

    Handout

--- junit-stuff.s
+++ junit-stuff.s
@@ -1,67 +1,52 @@
 .data
 nl:  .asciiz "\n"
 inputStr: .space 80
-ld35: .asciiz "Division: "
-lm35: .asciiz "Multiplikation: "
-ls35: .asciiz "Subtraktion: "
-la35: .asciiz "Addition: "
-zahl234: .word 0
-zahl134: .word 0
+klAls35: .asciiz "Sie solltens doch wisse, diese Zahl ist nicht 3!"
+thx35: .asciiz "Vielen Dank für die korrekte Eingabe."
+i34: .word 0
+l35: .asciiz "Bitte geben sie eine 3 ein:"
+grAls35: .asciiz "Ihre Zahl ist grösser als 3"
 .text
 main:
-la $a0, 150
-la $t9, zahl134
+la $a0, 0
+la $t9, i34
 sw $a0, 0($t9)
-la $a0, 50
-la $t9, zahl234
+while55:
+lw $a1, i34
+la $a2, 3
+sne $a0, $a1, $a2
+beqz $a0, endwhile55
+la $a0, l35
+li $v0, 4
+syscall
+move $t9, $a0
+la $a0, nl
+li $v0, 4
+syscall
+move $a0, $t9
+li $v0, 5
+la $a0, inputStr
+syscall
+move $a0, $v0
+la $t9, i34
 sw $a0, 0($t9)
-la $a0, la35
+lw $a1, i34
+la $a2, 3
+slt $a0, $a1, $a2
+beq $a0, 1,if76
+lw $a1, i34
+la $a2, 8
+sgt $a0, $a1, $a2
+beq $a0, 1,if88
+lw $a1, i34
+la $a2, 3
+sgt $a0, $a1, $a2
+beq $a0, 1,if101
+cont76:
+jal while55
+endwhile55:
+la $a0, thx35
 li $v0, 4
-syscall
-lw $a1, zahl134
-lw $a2, zahl234
-add $a0, $a1, $a2
-li $v0, 1
-syscall
-move $t9, $a0
-la $a0, nl
-li $v0, 4
-syscall
-move $a0, $t9
-la $a0, ls35
-li $v0, 4
-syscall
-lw $a1, zahl134
-lw $a2, zahl234
-neg $a2, $a2
-add $a0, $a1, $a2
-li $v0, 1
-syscall
-move $t9, $a0
-la $a0, nl
-li $v0, 4
-syscall
-move $a0, $t9
-la $a0, lm35
-li $v0, 4
-syscall
-lw $a1, zahl134
-lw $a2, zahl234
-mul $a0, $a1, $a2
-li $v0, 1
-syscall
-move $t9, $a0
-la $a0, nl
-li $v0, 4
-syscall
-move $a0, $t9
-la $a0, ld35
-li $v0, 4
-syscall
-lw $a1, zahl134
-lw $a2, zahl234
-div $a0, $a1, $a2
-li $v0, 1
 syscall
 move $t9, $a0
 la $a0, nl
@@ -70,3 +55,31 @@ syscall
 move $a0, $t9
 li $v0, 10
 syscall
+
+nichtDrei:
+la $a0, klAls35
+li $v0, 4
+syscall
+move $t9, $a0
+la $a0, nl
+li $v0, 4
+syscall
+move $a0, $t9
+jr $ra
+
+if76:
+jal nichtDrei
+jal cont76
+if88:
+jal nichtDrei
+jal cont76
+if101:
+la $a0, grAls35
+li $v0, 4
+syscall
+move $t9, $a0
+la $a0, nl
+li $v0, 4
+syscall
+move $a0, $t9
+jal cont76
--- testClasses/SysIn.java
+++ testClasses/SysIn.java
@@ -13,7 +13,6 @@ public class SysIn {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String klAls = "Ihre Zahl ist kleiner als 3";
 		String grAls = "Ihre Zahl ist grösser als 3";
 		
 		String l = "Bitte geben sie eine 3 ein:";
@@ -23,7 +22,10 @@ public class SysIn {
 			i = Integer.valueOf(br.readLine());
 			
 			if(i < 3) {
-				System.out.println(klAls);
+				nichtDrei();
+			}
+			else if(i > 8) {
+				nichtDrei();
 			}
 			else if(i > 3) {
 				System.out.println(grAls);
@@ -33,5 +35,12 @@ public class SysIn {
 		String thx = "Vielen Dank für die korrekte Eingabe.";
 		System.out.println(thx);
 		
+	}
+	
+	private static void nichtDrei() {
+
+		String klAls = "Sie solltens doch wisse, diese Zahl ist nicht 3!";
+
+		System.out.println(klAls);
 	}
 }
